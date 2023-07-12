24a067603bb64a07cc6ff24001d7d973f669d7aa
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-07-12 21:05:08 +0600

    added try-except, fixed item_link adding

--- main.py
+++ main.py
@@ -8,40 +8,46 @@ URL = 'https://www.technodom.kz/catalog/'
 
 
 def get_catalogue_links(url: str) -> list[str]:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    li_tags = soup.find_all('li', attrs='catalog-page__subcategory')
-    fetched_urls = []
+        li_tags = soup.find_all('li', attrs='catalog-page__subcategory')
+        fetched_urls = []
 
-    for li in li_tags:
-        a = li.find('a')
-        href = a.get('href')
-        fetched_urls.append(DOMAIN + href)
+        for li in li_tags:
+            a = li.find('a')
+            href = a.get('href')
+            fetched_urls.append(DOMAIN + href)
 
-    return fetched_urls
+        return fetched_urls
+    except Exception as e:
+        print("Error: ", str(e))
 
 
 def get_products_links(url: str) -> list[str]:
     page = 1
     fetched_urls = []
     while True:
-        page_url = url + f'?page={page}'
-        print(page_url)
-        response = requests.get(page_url)
+        try:
+            page_url = url + f'?page={page}'
+            print(page_url)
+            response = requests.get(page_url)
 
-        soup = BeautifulSoup(response.content, 'html.parser')
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        if len(soup.find_all('a', attrs='category-page-list__item-link')) == 0:
-            return fetched_urls
+            if len(soup.find_all('a', attrs='category-page-list__item-link')) == 0:
+                return fetched_urls
 
-        ul_tags = soup.find('ul', attrs='category-page-list__list')
-        links = ul_tags.find_all('a')
-        for link in links:
-            href = link.get('href')
-            fetched_urls.append(DOMAIN + href)
+            ul_tags = soup.find('ul', attrs='category-page-list__list')
+            links = ul_tags.find_all('a')
+            for link in links:
+                href = link.get('href')
+                fetched_urls.append(DOMAIN + href)
 
-        page += 1
+            page += 1
+        except Exception as e:
+            print("Error: ", str(e))
 
 
 def get_all_products(url: str) -> list[dict]:
@@ -49,25 +55,32 @@ def get_all_products(url: str) -> list[dict]:
     items = []
     for link in catalogue_links:
         print(link)
-        response = requests.get(link)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        item = {}
+        try:
+            response = requests.get(link)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            item = {}
 
-        span_tags = soup.find_all('span', itemprop='name')
-        if len(span_tags) == 4:
-            item['category'] = span_tags[1].text
-            item['subcategory'] = span_tags[2].text
-            item['item_type'] = span_tags[3].text
-        elif len(span_tags) == 3:
-            item['category'] = span_tags[1].text
-            item['subcategory'] = span_tags[2].text
-        elif len(span_tags) == 2:
-            item['category'] = span_tags[1].text
+            span_tags = soup.find_all('span', itemprop='name')
+            if len(span_tags) == 4:
+                item['category'] = span_tags[1].text
+                item['subcategory'] = span_tags[2].text
+                item['item_type'] = span_tags[3].text
+            elif len(span_tags) == 3:
+                item['category'] = span_tags[1].text
+                item['subcategory'] = span_tags[2].text
+            elif len(span_tags) == 2:
+                item['category'] = span_tags[1].text
 
-        item_links = get_products_links(link)
-        for item_link in item_links:
-            item['item_link'] = item_link
-            items.append(item)
+            item_links = get_products_links(link)
+
+            if not item_links:
+                continue
+
+            for item_link in item_links:
+                items.append({**item, 'item_link': item_link})
+
+        except Exception as e:
+            print("Error: ", str(e))
 
     return items
 
