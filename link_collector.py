e90715aff8e7aba4fe9944e2291b435f8609ffd2
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-07-23 01:31:34 +0600

    reducing try except blocks and adding logging

--- link_collector.py
+++ link_collector.py
@@ -1,5 +1,9 @@
+import logging
+
 import requests
 from bs4 import BeautifulSoup
+
+logging.basicConfig(level=logging.ERROR, filename='logs.log', filemode='w')
 
 
 class LinkCollector:
@@ -11,39 +15,48 @@ class LinkCollector:
         try:
             response = requests.get(self.url)
             soup = BeautifulSoup(response.content, 'html.parser')
-
-            li_tags = soup.find_all('li', attrs='catalog-page__subcategory')
-            fetched_urls = []
-
-            for li in li_tags:
-                a = li.find('a')
-                href = a.get('href')
-                fetched_urls.append(self.domain + href)
-
-            return fetched_urls
         except Exception as e:
-            print("Error: ", str(e))
+            logging.error(str(e))
+
+        try:
+            li_tags = soup.find_all('li', attrs='catalog-page__subcategory')
+        except AttributeError as e:
+            logging.error(str(e))
+            li_tags = []
+
+        fetched_urls = []
+        for li in li_tags:
+            a = li.find('a')
+            href = a.get('href')
+            fetched_urls.append(self.domain + href)
+
+        return fetched_urls
 
     def get_products_links(self, page_link: str) -> list[str]:
         page = 1
         fetched_urls = []
         while True:
+            page_url = page_link + f'?page={page}'
+            print(page_url)
+
             try:
-                page_url = page_link + f'?page={page}'
-                print(page_url)
                 response = requests.get(page_url)
-
                 soup = BeautifulSoup(response.content, 'html.parser')
+            except Exception as e:
+                logging.error(str(e))
 
-                if len(soup.find_all('a', attrs='category-page-list__item-link')) == 0:
-                    return fetched_urls
+            if len(soup.find_all('a', attrs='category-page-list__item-link')) == 0:
+                return fetched_urls
 
+            try:
                 ul_tags = soup.find('ul', attrs='category-page-list__list')
                 links = ul_tags.find_all('a')
-                for link in links:
-                    href = link.get('href')
-                    fetched_urls.append(self.domain + href)
+            except AttributeError as e:
+                logging.error(str(e))
+                links = []
 
-                page += 1
-            except Exception as e:
-                print("Error: ", str(e))
+            for link in links:
+                href = link.get('href')
+                fetched_urls.append(self.domain + href)
+
+            page += 1
--- link_handler.py
+++ link_handler.py
@@ -1,10 +1,13 @@
 import json
+import logging
 import os
 
 import requests
 from bs4 import BeautifulSoup
 
 from link_collector import LinkCollector
+
+logging.basicConfig(level=logging.ERROR, filename='logs.log', filemode='w')
 
 
 class LinkHandler:
@@ -16,42 +19,42 @@ class LinkHandler:
         catalogue_links = self.collector.get_catalogue_links()
         for link in catalogue_links:
             print(link)
+
             try:
                 response = requests.get(link)
                 soup = BeautifulSoup(response.content, 'html.parser')
-                items = []
-                item = {}
-
-                span_tags = soup.find_all('span', itemprop='name')
-                if len(span_tags) == 4:
-                    item['category'] = span_tags[1].text
-                    item['subcategory'] = span_tags[2].text
-                    item['item_type'] = span_tags[3].text
-                elif len(span_tags) == 3:
-                    item['category'] = span_tags[1].text
-                    item['subcategory'] = span_tags[2].text
-                elif len(span_tags) == 2:
-                    item['category'] = span_tags[1].text
-
-                item_links = self.collector.get_products_links(link)
-
-                if not item_links:
-                    continue
-
-                for item_link in item_links:
-                    items.append({**item, 'item_link': item_link})
-
-                if not os.path.exists(self.file_path):
-                    with open(self.file_path, 'w') as outfile:
-                        json.dump(items, outfile, indent=6)
-                else:
-                    with open(self.file_path, 'r') as outfile:
-                        data = json.load(outfile)
-
-                    data.append(items)
-
-                    with open(self.file_path, 'w') as outfile:
-                        json.dump(data, outfile, indent=6)
-
             except Exception as e:
-                print("Error: ", str(e))
+                logging.error(str(e))
+
+            items = []
+            item = {}
+            span_tags = soup.find_all('span', itemprop='name')
+            if len(span_tags) == 4:
+                item['category'] = span_tags[1].text
+                item['subcategory'] = span_tags[2].text
+                item['item_type'] = span_tags[3].text
+            elif len(span_tags) == 3:
+                item['category'] = span_tags[1].text
+                item['subcategory'] = span_tags[2].text
+            elif len(span_tags) == 2:
+                item['category'] = span_tags[1].text
+
+            item_links = self.collector.get_products_links(link)
+
+            if not item_links:
+                continue
+
+            for item_link in item_links:
+                items.append({**item, 'item_link': item_link})
+
+            if not os.path.exists(self.file_path):
+                with open(self.file_path, 'w') as outfile:
+                    json.dump(items, outfile, indent=6)
+            else:
+                with open(self.file_path, 'r') as outfile:
+                    data = json.load(outfile)
+
+                data.append(items)
+
+                with open(self.file_path, 'w') as outfile:
+                    json.dump(data, outfile, indent=6)
